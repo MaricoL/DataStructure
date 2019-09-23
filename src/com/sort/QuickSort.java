b1df75991c9fa4a624737b8f14b3b95c39bc3fe5
mrjimmylin <mrjimmylin@163.com>
2019-09-23 21:38:48 +0800

    init

--- src/com/sort/QuickSort.java
+++ src/com/sort/QuickSort.java
@@ -1,4 +1,40 @@
 package com.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        quickSort(nums , 0 , nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void quickSort(int[] nums, int start, int end) {
+        if(start >= end) return;
+        int p = partition(nums, start, end);
+        quickSort(nums, start, p - 1);
+        quickSort(nums , p + 1 , end);
+    }
+
+    private static int partition(int[] nums, int start, int end) {
+        int pivot = nums[end];
+        int i = start;
+        for (int j = start; j < nums.length; j++) {
+            if (nums[j] < pivot) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, end);
+
+        return i;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
