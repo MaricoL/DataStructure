a99e33930b92d4ce6b5161ae3a43585c5eb7c4f0
mrjimmylin <mrjimmylin@163.com>
2019-09-26 08:43:08 +0800

    update

---
+++ src/com/sort4/MergeSort.java
@@ -0,0 +1,42 @@
+package com.sort4;
+
+import java.util.Arrays;
+
+/**
+ * @author mrjimmylin
+ * @description 归并排序
+ * @date 2019/9/26 8:14
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        mergeSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void mergeSort(int[] nums, int start, int end) {
+        if (start >= end) return;
+        int mid = (start + end) / 2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
+    private static void merge(int[] nums, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = 0;
+        int p1 = start , p2 = mid + 1;
+        while (p1 <= mid && p2 <= end) {
+            temp[i++] = nums[p1] <= nums[p2] ? nums[p1++] : nums[p2++];
+        }
+        while (p1 <= mid) {
+            temp[i++] = nums[p1++];
+        }
+        while (p2 <= end) {
+            temp[i++] = nums[p2++];
+        }
+        for (i = 0; i < temp.length; i++) {
+            nums[start + i] = temp[i];
+        }
+    }
+}
---
+++ src/com/sort4/QuickSort.java
@@ -0,0 +1,42 @@
+package com.sort4;
+
+import java.util.Arrays;
+
+/**
+ * @author mrjimmylin
+ * @description 快速排序
+ * @date 2019/9/26 8:23
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        quickSort(nums, 0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void quickSort(int[] nums, int start, int end) {
+        if (start >= end) return;
+        int pivot = partition(nums, start, end);
+        quickSort(nums, start, pivot - 1);
+        quickSort(nums, pivot + 1, end);
+    }
+
+    private static int partition(int[] nums, int start, int end) {
+        int pivotNum = nums[end];
+        int i = start;
+        for (int j = start; j < nums.length; j++) {
+            if (nums[j] < pivotNum) {
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, end);
+        return i;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
